19f1a3025044ca07d30c8fe05f87dfd53e8c5438
Nicholas Bloor <161645384+nbloor@users.noreply.github.com>
2024-09-26 11:13:59 -0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -5,3 +5,10 @@ st.write("This is a simple Streamlit app.")
 
 user_input = st.text_input("Enter your name.")
 st.write("Your name is", user_input)
+
+age = st.slider("Choose your age", 1, 100)
+st.write("You are", age, " years old")
+
+if st.button('Say hello'):
+  st.write("Hello there.")
+
